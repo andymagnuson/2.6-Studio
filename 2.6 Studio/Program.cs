89f9b043be402714a515eb97df1cbad201417722
Andy Magnuson <andymagnuson@gmail.com>
2020-06-02 11:30:48 -0400

    Add project files.

---
+++ 2.6 Studio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2._6_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius of a circle:");
+            int radius = Int32.Parse(Console.ReadLine());
+            conso
+        }
+    }
+}
